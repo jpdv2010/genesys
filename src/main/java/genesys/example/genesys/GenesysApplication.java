e9a5866743c2f2cc40dfeb2c895f7dd5400ca28a
jpdv2010 <joaopaulodv2011@gmail.com>
2017-10-20 15:50:30 -0200

    first commit

---
+++ src/main/java/genesys/example/genesys/GenesysApplication.java
@@ -0,0 +1,12 @@
+package genesys.example.genesys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenesysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenesysApplication.class, args);
+	}
+}
