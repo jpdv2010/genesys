d6992a642fd33d1f25098418a518d0921fd821bf
Thiago M <thiagopermuniz@gmail.com>
2017-10-25 09:32:59 -0200

    html customizado com jq e bs

--- IdeaProjects/devops/src/main/java/com/friik/devops/web/controllers/IndexController.java
+++ IdeaProjects/devops/src/main/java/com/friik/devops/web/controllers/IndexController.java
@@ -2,6 +2,7 @@ package com.friik.devops.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class IndexController {
